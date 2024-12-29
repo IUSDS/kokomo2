a70f0368d5482d1f65942f05b53027f15edb3baa
Nainika Singh <141204621+nainikasingh@users.noreply.github.com>
2024-12-29 17:18:41 +0530

    Update validate_user.py

--- routes/validate_user.py
+++ routes/validate_user.py
@@ -3,6 +3,21 @@ from pydantic import BaseModel
 from database import get_db_connection
 from models import User, UserResponse
 import pymysql
+from typing import Optional
+
+class User(BaseModel):
+    USER: str
+    password: str
+
+class UserResponse(BaseModel):
+    member_id: int
+    full_name: str
+    membership_type: str
+    points: int
+    picture_url: Optional[str]
+    address: set
+    email_id: str
+    phone_number: int
 
 validate_user_route = APIRouter()
 
