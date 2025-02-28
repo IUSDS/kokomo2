4c93f1ec884398913e76649699873a27cc6fc1f4
Satyajeet Sau <sausatyajeet100@gmail.com>
2025-02-28 17:58:15 +0530

    Update create_member.py

--- routes/create_member.py
+++ routes/create_member.py
@@ -10,27 +10,19 @@ from fastapi import  Depends, HTTPException
 from datetime import datetime
 from typing import List,  Optional, Union
 from emails.welcome_email import send_welcome_email, generate_temp_password
-#from PIL import Image
-#import io
-#import pillow_heif
 
 # Initialize router
 create_member_route = APIRouter()
-
 # Password hashing context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 # AWS S3 Configuration
 S3_BUCKET_NAME = "image-bucket-kokomo-yacht-club"
 S3_REGION = "ap-southeast-2"
 ACCESS_POINT_ALIAS = "first-kokomo-access-y1pahkde96c1mfspxs7cbiaro94hyaps2a-s3alias"
-
 # Initialize S3 client
 s3_client = boto3.client("s3", region_name=S3_REGION)
-
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
-
 @create_member_route.post("/add-member/") 
 async def add_member(
     request: Request,
@@ -69,7 +61,6 @@ async def add_member(
     type_of_acc: str = Form(None),
     date_sub: str = Form(default=datetime.utcnow().strftime('%Y-%m-%d')),
     #existing_membership_id: Union[int, None] = Form(None),  # New field to allow linking to an existing membership
-
     # Adding children details
     child_names: List[str] = Form([]),
     child_dobs: List[str] = Form([]),
@@ -92,7 +83,6 @@ async def add_member(
         cursor.execute("SELECT COUNT(*) AS count FROM Members WHERE email_id = %s", (email_id,))
         if cursor.fetchone()["count"] > 0:
             raise HTTPException(status_code=400, detail="Account already exists for this email, try logging in.")
-
         # Assign Membership ID (either existing or new)
         #if existing_membership_id:
             #membership_id = existing_membership_id
@@ -103,40 +93,13 @@ async def add_member(
         temp_password = generate_temp_password()
         hashed_password = hash_password(temp_password)
 
-        # Process profile picture upload or assign default photo also Added heic
+        # Process profile picture upload or assign default photo
         if file and isinstance(file, UploadFile):
             if file.content_type not in ["image/jpeg", "image/png"]:
                 raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
             file_content = await file.read()
             object_name = f"profile_pictures/{username}/{file.filename}"
-                        
-            #file_content = await file.read()
-            #image_format = file.content_type.split("/")[-1].lower()
-
             try:
-                # Convert HEIC to JPEG
-                #if image_format == "heic":
-                 #   heif_file = pillow_heif.open_heif(io.BytesIO(file_content))
-                #  image = heif_file.to_pillow()  # Convert HEIC to PIL Image
-                #    image = image.convert("RGB")  # Ensure RGB mode
-                 #   image_format = "jpeg"
-                #else:
-                 #   image = Image.open(io.BytesIO(file_content))
-
-                # Compress image if larger than 1MB
-               # image_bytes_io = io.BytesIO()
-               # if len(file_content) > 1_000_000:  # 1MB
-               #     image.save(image_bytes_io, format=image_format.upper(), quality=55)
-                #    compressed_file_content = image_bytes_io.getvalue()
-                    
-                 #   if len(compressed_file_content) > 1_000_000:
-                  #      raise HTTPException(status_code=400, detail="Compressed image is still too large. Please upload a smaller image.")
-                #else:
-                 #   image.save(image_bytes_io, format=image_format.upper())
-                  #  compressed_file_content = image_bytes_io.getvalue()
-
-                # Upload image to S3
-#                object_name = f"profile_pictures/{file.filename}"
                 s3_client.put_object(
                     Bucket=S3_BUCKET_NAME,
                     Key=object_name,
@@ -144,13 +107,12 @@ async def add_member(
                     ContentType=file.content_type,
                 )
                 picture_url = f"https://{ACCESS_POINT_ALIAS}.s3.{S3_REGION}.amazonaws.com/{object_name}"
-
             except ClientError as e:
                 raise HTTPException(status_code=500, detail=f"S3 Upload error: {e.response['Error']['Message']}")
         else:
             # Either file was not provided or an empty string was sent
-            picture_url = "https://image-bucket-kokomo-yacht-club.s3.ap-southeast-2.amazonaws.com/profile_pictures/default.png"                        
-                
+            picture_url = "https://image-bucket-kokomo-yacht-club.s3.ap-southeast-2.amazonaws.com/profile_pictures/default.png"
+
         # Insert member data
         query = """
             INSERT INTO Members (username, pass, first_name, last_name, phone_number, member_address1, member_address2, member_city, member_state, member_zip,
@@ -177,13 +139,11 @@ async def add_member(
         INSERT INTO Member_Emergency_Details (member_id, ec_name, ec_relationship, ec_phone_number, spouse, spouse_email, spouse_phone_number)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
         """, (member_id, emergency_name, emergency_relationship, emergency_contact, spouse or None, spouse_email or None, spouse_phone or None))
-
         # Insert bank details
         cursor.execute("""
         INSERT INTO Member_ACH_Details (member_id, depository, branch, city, state, zip, routing_no, acc_no, name_on_acc, type_of_acc, date_sub)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (member_id, depository_name, branch, city, state, zip_code, routing_no, acc_no, name_on_acc, type_of_acc, date_sub))
-
         # Insert children's details
         num_children = len(child_names)
         if num_children > 4:
@@ -202,9 +162,7 @@ async def add_member(
                 child_dobs[i] if child_dobs[i] else None,
             )
             cursor.execute(query, values)
-
         connection.commit()
-
         return {"status": "success", "message": "Member added successfully", "member_id": member_id, "email_status": email_response}
     
     except Exception as e:
@@ -225,24 +183,19 @@ async def validate_member(username: str = None, email_id: str = None):
     try:
         connection = get_db_connection()
         cursor = connection.cursor()
-
         if username:
             cursor.execute("SELECT COUNT(*) AS count FROM Members WHERE username = %s", (username,))
             if cursor.fetchone()["count"] > 0:
                 raise HTTPException(status_code=400, detail="Username already exists.")
-
         if email_id:
             cursor.execute("SELECT COUNT(*) AS count FROM Members WHERE email_id = %s", (email_id,))
             if cursor.fetchone()["count"] > 0:
                 raise HTTPException(status_code=400, detail="Email is already registered.")
-
         return {"status": "success", "message": "Username and email are available"}
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-
     finally:
         if "cursor" in locals():
             cursor.close()
         if "connection" in locals():
-            connection.close()
+connection.close()
