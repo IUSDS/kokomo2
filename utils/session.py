7d14938dc6f5974295c6154785547abecd4d8f7f
Satya <sausatyajeet100@gmail.com>
2025-04-29 17:43:21 +0530

    resolves member_id issue

--- utils/session.py
+++ utils/session.py
@@ -1,7 +1,37 @@
 from fastapi import Request, HTTPException
+from utils.database import get_db_connection
+from pymysql.cursors import DictCursor
 
 def get_logged_in_username(request: Request) -> str:
     username = request.session.get("username")
     if not username:
         raise HTTPException(status_code=401, detail="Session expired or unauthorized")
     return username
+
+def get_logged_in_member_id(request: Request) -> str:
+    username = get_logged_in_username(request)
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor(DictCursor)
+        
+        # Query to fetch member details based on the session (username)
+        cursor.execute("""
+            SELECT member_id
+            FROM Members 
+            WHERE LOWER(username) = LOWER(%s) AND is_deleted = 'N'
+            LIMIT 1
+        """, (username,))
+        member = cursor.fetchone()
+        
+        if not member:
+            raise HTTPException(status_code=404, detail="User not found.")
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database query error: {str(e)}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+            
+    return member["member_id"]
